618acbfb3534b548b1521a6fc47658ccc1cdd153
suman4545 <smn.bsws@gmail.com>
2018-08-22 00:36:03 +0530

    commit

--- src/main/java/com/soft/upload/component/controllers/GenericUploadController.java
+++ src/main/java/com/soft/upload/component/controllers/GenericUploadController.java
@@ -1,13 +1,21 @@
 package com.soft.upload.component.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.soft.upload.component.kafkaservices.KafkaSenderServiceimpl;
+import com.soft.upload.component.models.PartsDeatilsMap;
 import com.soft.upload.component.models.UploadFileResponse;
 import com.soft.upload.component.services.FileStorageService;
 
@@ -16,16 +24,31 @@ public class GenericUploadController {
 
 	@Autowired
 	FileStorageService FILE_SERVICE_OBJ;
-	
-	@PostMapping("/uploadFile")
-    public ResponseEntity<UploadFileResponse>  uploadFile(@RequestParam("file") MultipartFile file) {
-		UploadFileResponse res = FILE_SERVICE_OBJ.storeFile(file);
-		
-		return new ResponseEntity<UploadFileResponse>(res, HttpStatus.OK);
-		
-		
 
-      
-    }
-	
+	@Autowired
+	KafkaSenderServiceimpl KAFKASERVER_OBJ;
+
+	@PostMapping("/uploadFile")
+	// @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+
+	public ResponseEntity<UploadFileResponse> uploadFile(@ModelAttribute PartsDeatilsMap reqMapBody) {
+		System.out.println(reqMapBody.getPartMap());
+		ObjectMapper mapper = new ObjectMapper();
+		HashMap<String, String> reqMAp = new HashMap<String, String>();
+		// convert JSON string to Map
+		UploadFileResponse res = null;
+		try {
+			reqMAp = mapper.readValue(reqMapBody.getPartMap(), new TypeReference<Map<String, String>>() {});
+			KAFKASERVER_OBJ.sendkafkaMsg(reqMAp);
+			res = FILE_SERVICE_OBJ.storeFile(reqMapBody.getFile());
+			return new ResponseEntity<UploadFileResponse>(res, HttpStatus.OK);
+
+		} catch (Exception e) {
+
+			return new ResponseEntity<UploadFileResponse>(res, HttpStatus.OK);
+
+		}
+
+	}
+
 }
--- src/main/java/com/soft/upload/component/services/CreateUploadResponse.java
+++ src/main/java/com/soft/upload/component/services/CreateUploadResponse.java
@@ -1,5 +1,7 @@
 package com.soft.upload.component.services;
 
+import java.io.FileNotFoundException;
+
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,16 +13,21 @@ import com.soft.upload.component.models.UploadFileResponse;
 public class CreateUploadResponse {
 	
 	
-	public UploadFileResponse uploadfileResponse(MultipartFile file){
-		 String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-		
-		  String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
-	                .path("/downloadFile/")
-	                .path(fileName)
-	                .toUriString();
+	public UploadFileResponse uploadfileResponse(MultipartFile file) throws FileNotFoundException{
+		 String fileName = StringUtils.cleanPath(file.getOriginalFilename());		
+		 
+			
+			  String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+		                .path("/downloadFile/")
+		                .path(fileName)
+		                .toUriString();
 
-	        return new UploadFileResponse(fileName, fileDownloadUri,
-	                file.getContentType(), file.getSize());
+		        return new UploadFileResponse(fileName, fileDownloadUri,
+		                file.getContentType(), file.getSize());
+			
+		 
+		 
+	       
 		
 		
 	}
