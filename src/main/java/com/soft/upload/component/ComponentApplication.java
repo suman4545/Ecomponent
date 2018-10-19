d0d127a3c49debae2a6e0814be61548e58bcd461
suman4545 <smn.bsws@gmail.com>
2018-10-20 04:55:50 +0530

    new

--- src/main/java/com/soft/upload/component/ComponentApplication.java
+++ src/main/java/com/soft/upload/component/ComponentApplication.java
@@ -4,6 +4,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/*This Application Will get data from as  rest Service.
+ * Kafka server need to send the data 
+ * And FTP server Need to Store A File
+ * 
+ * 
+ * 
+ */
 @SpringBootApplication
 public class ComponentApplication {
 	
